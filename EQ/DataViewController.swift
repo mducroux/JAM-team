0afb4cd799af717e93de5a42d2382657f232b652
alexander-san <alexander.sanchez@epfl.ch>
2017-11-11 14:13:15 +0100

    added initial project structure (page based - empty)

---
+++ EQ/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  EQ
+//
+//  Created by Alexander Sanchez de la Cerda on 11.11.17.
+//  Copyright © 2017 Alexander Sanchez de la Cerda. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
